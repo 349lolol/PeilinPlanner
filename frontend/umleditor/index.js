c04ca66f7e76f7f7f93bbf6cb9ebefe327ad90fc
PerryXu1 <92761967+PerryXu1@users.noreply.github.com>
2024-01-13 11:40:21 -0500

    added movable and editable diagrams and home button to webpage

--- frontend/umleditor/index.js
+++ frontend/umleditor/index.js
@@ -8,34 +8,38 @@ grid.addEventListener("scroll", (event) => {
   }
 })
 
-const element = document.getElementById('diagram1');
-let x = 0;
-let y = 0
+const elements = document.querySelectorAll('.diagram');
 
-interact(element)
-  .draggable({
-    modifiers: [
-      interact.modifiers.snap({
-        targets: [
-          interact.snappers.grid({
-             x: 10,
-             y: 10, 
-          })
-        ],
-        range: Infinity,
-        relativePoints: [ { x: 0, y: 0 } ]
-      }),
-      interact.modifiers.restrict({
-        restriction: element.parentNode,
-        elementRect: { top: 0, left: 0, bottom: 1, right: 1 },
-        endOnly: true
-      })
-    ],
-    inertia: true
-  })
-  .on('dragmove', function (event) {
-    x += event.dx
-    y += event.dy
 
-    event.target.style.transform = 'translate(' + x + 'px, ' + y + 'px)'
-  })
+elements.forEach((element) => {
+  let x = 0;
+  let y = 0
+
+  interact(element)
+    .draggable({
+      modifiers: [
+        interact.modifiers.snap({
+          targets: [
+            interact.snappers.grid({
+              x: 10,
+              y: 10, 
+            })
+          ],
+          range: Infinity,
+          relativePoints: [ { x: 0, y: 0 } ]
+        }),
+        interact.modifiers.restrict({
+          restriction: element.parentNode,
+          elementRect: { top: 0, left: 0, bottom: 1, right: 1 },
+          endOnly: true
+        })
+      ],
+      inertia: true
+    })
+    .on('dragmove', function (event) {
+      x += event.dx
+      y += event.dy
+
+      event.target.style.transform = 'translate(' + x + 'px, ' + y + 'px)'
+    })
+})
