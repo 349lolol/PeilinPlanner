985a3c3ad742c03a072a3a0cec0e31fc4f39b513
PerryXu1 <92761967+PerryXu1@users.noreply.github.com>
2024-01-12 22:08:52 -0500

    finished css and snapping for uml editor webpage

---
+++ frontend/umleditor/index.js
@@ -0,0 +1,41 @@
+const grid = document.getElementById('grid');
+
+grid.addEventListener("scroll", (event) => {
+  console.log(event);
+
+  for (const child of grid.children) {
+    grid.style.scale = "";
+  }
+})
+
+const element = document.getElementById('diagram1');
+let x = 0;
+let y = 0
+
+interact(element)
+  .draggable({
+    modifiers: [
+      interact.modifiers.snap({
+        targets: [
+          interact.snappers.grid({
+             x: 10,
+             y: 10, 
+          })
+        ],
+        range: Infinity,
+        relativePoints: [ { x: 0, y: 0 } ]
+      }),
+      interact.modifiers.restrict({
+        restriction: element.parentNode,
+        elementRect: { top: 0, left: 0, bottom: 1, right: 1 },
+        endOnly: true
+      })
+    ],
+    inertia: true
+  })
+  .on('dragmove', function (event) {
+    x += event.dx
+    y += event.dy
+
+    event.target.style.transform = 'translate(' + x + 'px, ' + y + 'px)'
+  })
